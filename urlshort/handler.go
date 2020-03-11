771c4d87f86caedf4bbfe656a4a70f71f339f327
Rolando <rolando.umana@gmail.com>
2020-03-10 19:41:23 -0600

    Added YAMLHandler function to package

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -2,6 +2,8 @@ package urlshort
 
 import (
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -11,12 +13,16 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+
 	return func(w http.ResponseWriter, r *http.Request) {
 
+		//if path is in the map, redirect to its URL
 		if url, ok := pathsToUrls[r.URL.Path]; ok {
 			http.Redirect(w, r, url, http.StatusFound)
 			return
 		}
+
+		//otherwise serve fallback
 		fallback.ServeHTTP(w, r)
 	}
 }
@@ -38,8 +44,41 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return func(w http.ResponseWriter, r *http.Request) {
-		fallback.ServeHTTP(w, r)
-	}, nil
+
+	routes, err := parseYAML(yml)
+	if err != nil {
+		return nil, err
+	}
+
+	return MapHandler(buildMap(routes), fallback), nil
+}
+
+//parseYAML converts the YAML structure into an array of route struct
+func parseYAML(yml []byte) ([]route, error) {
+	routes := []route{}
+
+	err := yaml.Unmarshal(yml, &routes)
+
+	return routes, err
+}
+
+//buildMap receives an array of route struct and creates a map
+//that later on we pass to the MapHandler function
+func buildMap(routes []route) map[string]string {
+
+	m := make(map[string]string)
+
+	for _, r := range routes {
+		m[r.Path] = r.URL
+	}
+
+	return m
+}
+
+//route holds the URL a given path needs to be redirected to
+type route struct {
+	//Path
+	Path string `yaml:"path"`
+	//URL ...
+	URL string `yaml:"url"`
 }
