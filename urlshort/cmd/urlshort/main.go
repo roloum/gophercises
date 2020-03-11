bcdfbc61e5165eb236c61ac72aaaf44a37945bad
Rolando <rolando.umana@gmail.com>
2020-03-10 20:35:22 -0600

    Added flag for YAML file

--- urlshort/cmd/urlshort/main.go
+++ urlshort/cmd/urlshort/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/roloum/gophercises/urlshort"
 )
 
 func main() {
+
+	var yamlFile string
+	flag.StringVar(&yamlFile, "yaml-file", "", "YAML File")
+	flag.Parse()
+
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -17,15 +24,31 @@ func main() {
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
+	var yaml []byte
+	var err error
+	if yamlFile == "" {
+
+		// Build the YAMLHandler using the mapHandler as the
+		// fallback
+		data := `
 - path: /urlshort
   url: https://github.com/gophercises/urlshort
 - path: /urlshort-final
   url: https://github.com/gophercises/urlshort/tree/solution
 `
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+		//convert data to byte array
+		yaml = []byte(data)
+	} else {
+		//read file
+		yaml, err = ioutil.ReadFile(yamlFile)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	//changed to avoid converting data to string and then back to byte array
+	//when yaml file is provided
+	yamlHandler, err := urlshort.YAMLHandler(yaml, mapHandler)
 	if err != nil {
 		panic(err)
 	}
